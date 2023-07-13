bb8bcf511464d0ff88969e8c258ec7d21ee4a555
kirisaki_vk <kirisaki.vk@gmail.com>
2023-07-13 16:54:31 +0300

    feat: added clear method to clear the terminal

--- todo_app/src/Prompt/PromptScanner.java
+++ todo_app/src/Prompt/PromptScanner.java
@@ -7,4 +7,8 @@ public class PromptScanner {
     public static Scanner get() {
         return scanner;
     }
+    public static void clear() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
 }
