ea12f6370aa48b419baf06ac4d59c2567f0c0901
kirisaki_vk <kirisaki.vk@gmail.com>
2023-07-13 16:53:25 +0300

    chore: better error management in DAO

--- todo_app/src/DAO/todo.java
+++ todo_app/src/DAO/todo.java
@@ -8,15 +8,20 @@ import java.util.List;
 
 public class todo {
 
-    public static void insert(Task task) throws SQLException {
-        PreparedStatement insertStatement = ConnectionProvider.getConnection().prepareStatement("INSERT INTO todo(title, description, deadline, priority, done) VALUES ( ?, ?, ?, ?, ?);");
-        insertStatement.setString(1, task.title());
-        insertStatement.setString(2, task.description());
-        insertStatement.setTimestamp(3, task.deadline());
-        insertStatement.setInt(4, task.priority());
-        insertStatement.setBoolean(5, task.done());
-        insertStatement.execute();
-        insertStatement.close();
+    public static void insert(Task task) {
+        try {
+            PreparedStatement insertStatement = ConnectionProvider.getConnection().prepareStatement("INSERT INTO todo(title, description, deadline, priority, done) VALUES ( ?, ?, ?, ?, ?);");
+            insertStatement.setString(1, task.title());
+            insertStatement.setString(2, task.description());
+            insertStatement.setTimestamp(3, task.deadline());
+            insertStatement.setInt(4, task.priority());
+            insertStatement.setBoolean(5, task.done());
+            insertStatement.execute();
+            insertStatement.close();
+        } catch (SQLException e) {
+            System.out.println("erreur au niveau de l'execution de la requête" +
+                    "\nMessage: " + e.getMessage());
+        }
     }
 
     public static List<Task> getAll() {
@@ -35,50 +40,71 @@ public class todo {
             }
             result.close();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            System.out.println("erreur au niveau de l'execution de la requête" +
+                    "\nMessage: " + e.getMessage());
         }
 
         return tasksList;
     }
 
-    public static Task get(int id) throws SQLException {
+    public static Task get(int id) {
         String query = "SELECT * FROM todo WHERE id=?;";
-        PreparedStatement statement = ConnectionProvider
-                .getConnection()
-                .prepareStatement(query);
-        statement.setInt(1, id);
-        ResultSet result = statement.executeQuery();
-        statement.close();
-        result.next();
-        String name = result.getString("title");
-        String description = result.getString("description");
-        Timestamp deadline = result.getTimestamp("deadline");
-        int priority = result.getInt("priority");
-        Boolean done = result.getBoolean("done");
-        result.close();
+        String description = null;
+        String name = null;
+        Timestamp deadline = null;
+        int priority = 0;
+        Boolean done = null;
+        try {
+            PreparedStatement statement = ConnectionProvider
+                    .getConnection()
+                    .prepareStatement(query);
+            statement.setInt(1, id);
+            ResultSet result = statement.executeQuery();
+            statement.close();
+            result.next();
+            name = result.getString("title");
+            description = result.getString("description");
+            deadline = result.getTimestamp("deadline");
+            priority = result.getInt("priority");
+            done = result.getBoolean("done");
+            result.close();
+        } catch (SQLException e) {
+            System.out.println("erreur au niveau de l'execution de la requête" +
+                    "\nMessage: " + e.getMessage());
+        }
         return new Task(id, name, description, deadline, priority, done);
     }
 
-    public static int update(int id, Boolean done) throws SQLException {
+    public static void update(int id, Boolean done) {
         String query = "UPDATE todo SET done=? WHERE id=?;";
-        PreparedStatement statement = ConnectionProvider
-                .getConnection()
-                .prepareStatement(query);
-        statement.setBoolean(1, done);
-        statement.setInt(2, id);
-        return statement.executeUpdate();
+        try {
+            PreparedStatement statement = ConnectionProvider
+                    .getConnection()
+                    .prepareStatement(query);
+            statement.setBoolean(1, done);
+            statement.setInt(2, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("erreur au niveau de l'execution de la requête" +
+                    "\nMessage: " + e.getMessage());
+        }
     }
 
-    public static void remove(int id) throws SQLException {
+    public static void remove(int id) {
         String query = "DELETE FROM todo WHERE id=?";
-        PreparedStatement statement = ConnectionProvider
-                .getConnection()
-                .prepareStatement(query);
-        statement.setInt(1, id);
-        statement.executeUpdate();
+        try {
+            PreparedStatement statement = ConnectionProvider
+                    .getConnection()
+                    .prepareStatement(query);
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e){
+            System.out.println("erreur au niveau de l'execution de la requête" +
+                    "\nMessage: " + e.getMessage());
+        }
     }
 
-    public static List<Task> get(Boolean done) throws SQLException {
+    public static List<Task> get(Boolean done) {
         List<Task> tasksList = new ArrayList<>();
         String query = "SELECT * FROM todo WHERE done="+done+";";
         try {
@@ -94,29 +120,35 @@ public class todo {
             }
             result.close();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            System.out.println("erreur au niveau de l'execution de la requête" +
+                    "\nMessage: " + e.getMessage());
         }
         return tasksList;
     }
 
-    public static List<Task> getToday() throws SQLException {
+    public static List<Task> getToday() {
         List<Task> tasksList = new ArrayList<>();
         String query = "DELETE FROM todo WHERE date_part(deadline)=current_date";
-        PreparedStatement statement = ConnectionProvider
-                .getConnection()
-                .prepareStatement(query);
-        ResultSet result = statement.executeQuery();
-        statement.close();
-        while (result.next()) {
-            int id = result.getInt("id");
-            String name = result.getString("title");
-            String description = result.getString("description");
-            Timestamp deadline = result.getTimestamp("deadline");
-            int priority = result.getInt("priority");
-            Boolean done = result.getBoolean("done");
-            tasksList.add(new Task(id, name, description, deadline, priority, done));
+        try {
+            PreparedStatement statement = ConnectionProvider
+                    .getConnection()
+                    .prepareStatement(query);
+            ResultSet result = statement.executeQuery();
+            statement.close();
+            while (result.next()) {
+                int id = result.getInt("id");
+                String name = result.getString("title");
+                String description = result.getString("description");
+                Timestamp deadline = result.getTimestamp("deadline");
+                int priority = result.getInt("priority");
+                Boolean done = result.getBoolean("done");
+                tasksList.add(new Task(id, name, description, deadline, priority, done));
+            }
+            result.close();
+        } catch (SQLException e) {
+            System.out.println("erreur au niveau de l'execution de la requête" +
+                    "\nMessage: " + e.getMessage());
         }
-        result.close();
         return tasksList;
     }
 }
