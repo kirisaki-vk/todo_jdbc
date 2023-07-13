4eace7a06874a06a091eb4724d836792f8464df8
kirisaki_vk <kirisaki.vk@gmail.com>
2023-07-13 11:40:07 +0300

    feat: added actual init

--- todo_app/src/Main.java
+++ todo_app/src/Main.java
@@ -1,5 +1,41 @@
+import Prompt.PromptScanner;
+import Prompt.UI;
+import DAO.Task;
+import DAO.todo;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException, InterruptedException {
+        while (true) {
+            UI.showMenu();
+            int choice = PromptScanner.get().nextInt();
+            switch (choice) {
+                case 1:
+                    Task newTask = UI.addTask();
+                    todo.insert(newTask);
+                    break;
+
+                case 2:
+                    UI.removeTask();
+                    break;
+
+                case 3:
+                    UI.updateTask();
+                    break;
+                case 4:
+                    UI.showTasks(todo.get(true));
+                    break;
+                case 5:
+                    UI.showTasks(todo.get(false));
+                    break;
+                case 6:
+                    UI.showTasks(todo.getToday());
+                    break;
+                case 7:
+                    System.exit(0);
+                    break;
+            }
+        }
     }
 }
